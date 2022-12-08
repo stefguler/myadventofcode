1dce63cfbe2835a8515161d1a848edd68810a92f
Stef <stef.guler@gmail.com>
2022-12-08 16:24:22 +0100

    start day4 part 1

--- 04_day4_1.ts
+++ 04_day4_1.ts
@@ -1,5 +1,32 @@
 import { syncReadFile } from "./00_readInput";
 
-const rucksackItems: string[] = syncReadFile('./day3File.txt')
+const pairList: string[] = syncReadFile('./day4File.txt')
 
 
+let result:number = 0;
+
+const pairs: string[] = pairList[0].split(',')
+const firstSections: string[] = pairs[0].split('-')
+const secondSections: string[] = pairs[1].split('-')
+
+const firstArr: number[] = [];
+const secondArr: number[] = [];
+
+
+for (let i:number = parseInt(firstSections[0]); i<=  parseInt(firstSections[1]); i++ ) {
+  firstArr.push(i)
+}
+
+for (let i:number = parseInt(secondSections[0]); i<=  parseInt(secondSections[1]); i++ ) {
+  secondArr.push(i)
+}
+
+const intersection = firstArr.filter(x => secondArr.includes(x))
+
+
+
+
+console.log("pairs", pairs)
+console.log("first arr: ", firstArr)
+console.log("second arr: ", secondArr)
+console.log("intersection: ", intersection)
