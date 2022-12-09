a3d530183fe4d1c008ad23519b21b2973dfc2ce7
Stef <stef.guler@gmail.com>
2022-12-09 18:00:59 +0100

    update day 4 part 1

---
+++ 04_day4_1.js
@@ -0,0 +1,50 @@
+"use strict";
+exports.__esModule = true;
+var _00_readInput_1 = require("./00_readInput");
+var pairList = (0, _00_readInput_1.syncReadFile)("./day4File.txt");
+var getSections = function (pairList) {
+    //split the string info into sections
+    var pairs = pairList.split(",");
+    var firstSections = pairs[0].split("-");
+    var secondSections = pairs[1].split("-");
+    //create two new arrays to use the include functions later
+    var firstArr = [];
+    var secondArr = [];
+    for (var i = parseInt(firstSections[0]); i <= parseInt(firstSections[1]); i++) {
+        firstArr.push(i);
+    }
+    for (var i = parseInt(secondSections[0]); i <= parseInt(secondSections[1]); i++) {
+        secondArr.push(i);
+    }
+    //push both arrays in a new array with first & second array
+    var sections = [firstArr, secondArr];
+    return sections;
+};
+var isAlreadyCovered = function (arr1, arr2) {
+    var notIncludedFound = false;
+    for (var i = arr2[0]; i <= arr2.at(-1); i++) {
+        if (!arr1.includes(i)) {
+            notIncludedFound = true;
+            break;
+        }
+    }
+    if (!notIncludedFound)
+        return true;
+    for (var i = arr1[0]; i <= arr1.at(-1); i++) {
+        if (!arr2.includes(i)) {
+            return false;
+        }
+    }
+    return true;
+};
+var runProgram = function (list) {
+    var result = 0;
+    for (var i = 0; i < list.length; i++) {
+        var sections = getSections(list[i]);
+        var isCovered = isAlreadyCovered(sections[0], sections[1]);
+        isCovered ? result++ : result;
+    }
+    console.log(result);
+    return result;
+};
+runProgram(pairList);
--- 04_day4_1.ts
+++ 04_day4_1.ts
@@ -1,32 +1,76 @@
 import { syncReadFile } from "./00_readInput";
 
-const pairList: string[] = syncReadFile('./day4File.txt')
+const pairList: string[] = syncReadFile("./day4File.txt");
 
+const getSections = (pairList: string): number[][] => {
 
-let result:number = 0;
+//split the string info into sections
+const pairs: string[] = pairList.split(",");
+const firstSections: string[] = pairs[0].split("-");
+const secondSections: string[] = pairs[1].split("-");
 
-const pairs: string[] = pairList[0].split(',')
-const firstSections: string[] = pairs[0].split('-')
-const secondSections: string[] = pairs[1].split('-')
-
+//create two new arrays to use the include functions later
 const firstArr: number[] = [];
 const secondArr: number[] = [];
 
-
-for (let i:number = parseInt(firstSections[0]); i<=  parseInt(firstSections[1]); i++ ) {
-  firstArr.push(i)
+for (
+  let i: number = parseInt(firstSections[0]);
+  i <= parseInt(firstSections[1]);
+  i++
+) {
+  firstArr.push(i);
 }
 
-for (let i:number = parseInt(secondSections[0]); i<=  parseInt(secondSections[1]); i++ ) {
-  secondArr.push(i)
+for (
+  let i: number = parseInt(secondSections[0]);
+  i <= parseInt(secondSections[1]);
+  i++
+) {
+  secondArr.push(i);
 }
 
-const intersection = firstArr.filter(x => secondArr.includes(x))
+//push both arrays in a new array with first & second array
+const sections: number[][] = [firstArr, secondArr]
 
+return sections
 
+}
+const isAlreadyCovered = (arr1: number[], arr2: number[]): boolean => {
 
+  let notIncludedFound: boolean = false;
+  
+  for (let i:number = arr2[0]; i <= arr2.at(-1); i++) {
 
-console.log("pairs", pairs)
-console.log("first arr: ", firstArr)
-console.log("second arr: ", secondArr)
-console.log("intersection: ", intersection)
+    if (!arr1.includes(i)) {
+      notIncludedFound = true;
+      break;
+    }
+  }
+
+  if (!notIncludedFound) return true
+  
+    for (let i:number = arr1[0]; i <= arr1.at(-1); i++) {
+      if (!arr2.includes(i)) {
+        return false;
+      }
+    }
+
+    return true
+
+};
+
+const runProgram = (list: string[]) => {
+  let result: number = 0;
+
+  for (let i:number = 0; i<list.length; i++) {
+    const sections: number[][] = getSections(list[i])
+    const isCovered: boolean = isAlreadyCovered(sections[0], sections[1]);
+    isCovered ? result++ : result;
+  }
+
+  console.log(result);
+  return result;
+
+}
+
+runProgram(pairList);
